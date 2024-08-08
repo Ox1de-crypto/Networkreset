0551a05623d9f7fac2b90fb9fdc30f6f668bed15
Mahabub Subhany <66554413+Ox1de-crypto@users.noreply.github.com>
2024-08-08 13:54:14 +0800

    Add files via upload

---
+++ reset-network.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo Please wait Network Reset starting...
+
+rem Flush DNS
+echo Flushing DNS...
+ipconfig /flushdns
+echo Done.
+
+rem Register DNS
+echo Registering DNS...
+ipconfig /registerdns
+echo Done.
+
+rem Release IP
+echo Releasing IP...
+ipconfig /release
+echo Done.
+
+
+rem Renew IP
+echo Renewing IP...
+ipconfig /renew
+echo Done.
+
+rem Reset Winsock
+echo Resetting Winsock...
+netsh winsock reset
+echo Done.
