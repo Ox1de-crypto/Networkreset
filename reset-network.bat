4ec80e48715b50c804678bd65b0c35231949e37a
Mahabub Subhany <66554413+Ox1de-crypto@users.noreply.github.com>
2024-08-09 14:43:35 +0800

    Update reset-network.bat

--- reset-network.bat
+++ reset-network.bat
@@ -1,6 +1,24 @@
 @echo off
+setlocal
 
-echo Please wait Network Reset starting - Script by X1...
+:: Display a starting message
+echo Network Reset Toolkit for Windows 10 and later...
+echo Script by X1 
+echo.
+echo   __   __  __ 
+echo  "\ \ / / /_ |"
+echo  " \ V /   | |"
+echo   " > <    | |"
+echo   "/ . \   | |"
+echo  "/_/ \_\  |_|" 
+echo.
+echo Version 0.3
+echo Github @Ox1de-crypto
+echo ========================================
+echo Gathering information Please wait..
+echo.
+echo Network Reset started...
+echo.
 
 rem Flush DNS
 echo Flushing DNS...
@@ -11,19 +29,22 @@ rem Register DNS
 echo Registering DNS...
 ipconfig /registerdns
 echo Done.
-
+echo.
 rem Release IP
 echo Releasing IP...
 ipconfig /release
 echo Done.
 
-
+echo.
 rem Renew IP
 echo Renewing IP...
 ipconfig /renew
 echo Done.
-
+echo.
 rem Reset Winsock
 echo Resetting Winsock...
 netsh winsock reset
 echo Done.
+
+endlocal
+pause
